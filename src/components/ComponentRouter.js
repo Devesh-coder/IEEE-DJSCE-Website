c38854536c360be323aa7c35fa436d669e5d4672
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 00:04:12 +0530

    Created ComponentRouter and added routes for nav elements

--- src/components/ComponentRouter.js
+++ src/components/ComponentRouter.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutUs from './AboutUs/AboutUs';
+import Events from './Events/Events';
+import Home from './Home/Home';
+import Sponsors from './Sponsors/Sponsors';
 
 const ComponentRouter = () => {
   return (
     <div>
-
+      <Router>
+        <Routes>
+          <Route path={'/aboutUs'} element={<AboutUs />} />
+          <Route path={'/sponsors'} element={<Sponsors />} />
+          <Route path={'/events'} element={<Events />} />
+          <Route path={'/'} element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
