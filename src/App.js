8e577c45f8def0167565c3b3197bbea0ab7b5452
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 00:04:25 +0530

    Added ComponentRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import cx from "classnames";
 import styles from "./App.module.scss";
+import ComponentRouter from './components/ComponentRouter';
 
 function App() {
   return (
     <div className={cx(styles.App)}>
-      <p className={cx(styles.para)}>Hello this is just for example</p>
+      {/* <p className={cx(styles.para)}>Hello this is just for example</p> */}
+      <ComponentRouter />
     </div>
   );
 }
