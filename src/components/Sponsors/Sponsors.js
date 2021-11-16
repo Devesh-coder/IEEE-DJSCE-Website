a1cef98f4b149e56c7f6b71c905e144603bbb9d7
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 00:06:34 +0530

    Created Sponsors

---
+++ src/components/Sponsors/Sponsors.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../CommonComp/Nav/Nav'
+
+const Sponsors = () => {
+  return (
+    <div>
+      <Nav />
+      Sponsor
+    </div>
+  )
+}
+
+export default Sponsors
