c27c994a49a5910ca4049f2e6e57df8698bf6bc0
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 00:06:18 +0530

    Created Home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../CommonComp/Nav/Nav'
+import styles from "./Home.module.scss"
+
+const Home = () => {
+
+  document.body.style.backgroundColor = "black";
+
+  return (
+    <div>
+      <Nav />
+      Home
+    </div>
+  )
+}
+
+export default Home
