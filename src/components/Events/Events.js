d207b5992dbb1cee4c467b6dbb158086982e117c
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 00:06:05 +0530

    Created events

---
+++ src/components/Events/Events.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../CommonComp/Nav/Nav'
+
+const Events = () => {
+  return (
+    <div>
+      <Nav />
+      Events
+    </div>
+  )
+}
+
+export default Events
