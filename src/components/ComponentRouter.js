466aaa00ec682b806b9f182fd5f73fc7404be6e6
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 22:42:33 +0530

    Added new teams route

--- src/components/ComponentRouter.js
+++ src/components/ComponentRouter.js
@@ -4,6 +4,7 @@ import AboutUs from './AboutUs/AboutUs';
 import Events from './Events/Events';
 import Home from './Home/Home';
 import Sponsors from './Sponsors/Sponsors';
+import Team from './Team/Team';
 
 const ComponentRouter = () => {
   return (
@@ -13,6 +14,7 @@ const ComponentRouter = () => {
           <Route path={'/aboutUs'} element={<AboutUs />} />
           <Route path={'/sponsors'} element={<Sponsors />} />
           <Route path={'/events'} element={<Events />} />
+          <Route path={'/teams'} element={<Team />} />
           <Route path={'/'} element={<Home />} />
         </Routes>
       </Router>
