107bd72c7fcfb67f7b0bcd1fbc1f58517a95d4e6
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 11:35:21 +0530

    Added contextholder, shifting comporouter in former

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import cx from "classnames";
 import styles from "./App.module.scss";
 import ComponentRouter from './components/ComponentRouter';
+import ContextComponentHolder from './components/ContextComponentHolder';
 
 function App() {
   return (
     <div className={cx(styles.App)}>
       {/* <p className={cx(styles.para)}>Hello this is just for example</p> */}
-      <ComponentRouter />
+      <ContextComponentHolder />
     </div>
   );
 }
