e426feebe8b712fe7f0bb6dfd147459b762a1f46
Manan Doshi <manandoshi1301@gmail.com>
2021-11-17 22:43:25 +0530

    Added teams compnent

---
+++ src/components/Team/Team.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../CommonComp/Nav/Nav';
+import styles from "./Team.module.scss";
+
+const Team = () => {
+  return (
+    <div className={styles.teams}>
+      <Nav />
+    </div>
+  )
+}
+
+export default Team
