96deb699837474dfd95c6cca5e7a5d6beec49b30
Manan Doshi <manandoshi1301@gmail.com>
2021-11-19 02:42:25 +0530

    Added Profiles to sub folder

--- src/components/Team/Profile/Profile.js
+++ src/components/Team/Profile/Profile.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from "./Profile.module.scss";
-import sample from './sample.png'
 
-function Profile({name,title}) {
+function Profile({ name, title, img }) {
     return (
         <div className={styles.profile}>
             <div className={styles.profile__background}>
-                <img src={sample} alt=""></img>
+                <img src={img} alt=""></img>
             </div>
             <div className={styles.profile__name}>{name}</div>
             <div className={styles.profile__title}>{title}</div>
