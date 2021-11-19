5256b2a4327f5d3537bfad3ee28e2c1e897005f0
Manan Doshi <manandoshi1301@gmail.com>
2021-11-19 16:01:49 +0530

    Nav configured with font size and minute details

--- src/components/CommonComp/Nav/Nav.js
+++ src/components/CommonComp/Nav/Nav.js
@@ -12,10 +12,10 @@ const Nav = () => {
   const IEEELogoImg = baseData.navLogo;
 
   const navElements = [
-    { title: "About Us", linkTo: "aboutUs", className: "" },
-    { title: "Sponsors", linkTo: "sponsors", className: "" },
-    { title: "Events", linkTo: "events", className: "" },
-    { title: "Our Team", linkTo: "teams", className: "" },
+    { title: "About Us", linkTo: "aboutUs", className: cx(styles.navElement) },
+    { title: "Sponsors", linkTo: "sponsors", className: cx(styles.navElement) },
+    { title: "Our Team", linkTo: "teams", className: cx(styles.navElement) },
+    { title: "Events", linkTo: "events", className: cx(styles.navElement, styles.navEvents) },
   ]
 
   return (
@@ -34,7 +34,7 @@ const Nav = () => {
         <div className={cx(styles.secondNav)}>
           {
             navElements.map((element, key) => (
-              <Link to={`/${element.linkTo}`} key={key} className={cx(element.className)}>
+              <Link to={`/${element.linkTo}`} key={key} className={element.className}>
                 {element.title}
               </Link>
             ))
