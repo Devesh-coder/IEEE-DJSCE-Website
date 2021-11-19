eb32039248ce5de9614d1d0700ea9272ab24d45b
PrachetShah <prachetshah25@gmail.com>
2021-11-19 14:22:07 +0530

    added buttons

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import cx from "classnames";
 import Nav from '../CommonComp/Nav/Nav'
+import styles from './Events.module.scss'
 
 const Events = () => {
   return (
     <div>
-      <Nav />
-      Events
+      <Nav />      
+      <div className={cx(styles.Events)}>
+        <p>Events</p>
+        <div>
+          <button>2017</button>
+          <button>2018</button>
+          <button>2019</button>
+          <button>2020</button>
+          <button>2021</button>
+        </div>
+      </div>
+      
     </div>
   )
 }
