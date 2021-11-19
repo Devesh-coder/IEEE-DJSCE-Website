2a957b6f92b5067452349ee0b2bede97cb13f902
Manan Doshi <manandoshi1301@gmail.com>
2021-11-19 16:57:46 +0530

    Added progressbar and transition to nav

--- src/components/CommonComp/Nav/Nav.js
+++ src/components/CommonComp/Nav/Nav.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import cx from "classnames"
 import styles from "./Nav.module.scss"
 import { Context } from '../../ContextData'
+import ProgressBar from '../ProgressBar/ProgressBar'
 
 
 
@@ -10,6 +11,20 @@ const Nav = () => {
 
   const [baseData] = useContext(Context);
   const IEEELogoImg = baseData.navLogo;
+
+  const [isScrolled, handleIsScrolled] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 70) {
+      handleIsScrolled(true);
+    } else {
+      handleIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
 
   const navElements = [
     { title: "About Us", linkTo: "aboutUs", className: cx(styles.navElement) },
@@ -19,10 +34,10 @@ const Nav = () => {
   ]
 
   return (
-    <div className={styles.navDiv}>
+    <div className={cx(styles.navDiv, isScrolled ? cx(styles.nav_dark) : "")}>
+      <ProgressBar />
 
       <nav className={styles.nav}>
-
 
         <div className={cx(styles.firstNav)}>
           <Link to={'/'} className={styles.navLogo}>
