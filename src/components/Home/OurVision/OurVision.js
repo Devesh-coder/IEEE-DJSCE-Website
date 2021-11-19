95b8c7341c9642b9e37461972cc717665545504c
Manan Doshi <manandoshi1301@gmail.com>
2021-11-20 00:43:36 +0530

    Updated Codebase

--- src/components/Home/OurVision/OurVision.js
+++ src/components/Home/OurVision/OurVision.js
@@ -6,14 +6,38 @@ import Vision from "./Vision/Vision";
 
 
 export default function OurVision() {
-    const data = [img, "Capitalize", "We attempt to capitalize on the abundant talent and ideas and give others a platform to showcase their perceptions."];
+
+    const data = [
+        {
+            title: "Capitalize",
+            caption: "We attempt to capitalize on the abundant talent and ideas and give others a platform to showcase their perceptions.",
+            img: img,
+        },
+        {
+            title: "Capitalize",
+            caption: "We attempt to capitalize on the abundant talent and ideas and give others a platform to showcase their perceptions.",
+            img: img,
+        },
+        {
+            title: "Capitalize",
+            caption: "We attempt to capitalize on the abundant talent and ideas and give others a platform to showcase their perceptions.",
+            img: img,
+        },
+    ]
+
     return (
-        <div>
+        <div className={styles.baseVision}>
             <h1 className={styles.OurVision}>Our Vision</h1>
             <div className={styles.Visions}>
-            <Vision img={data[0]} title={data[1]} caption={data[2]}/>
-            <Vision img={data[0]} title={data[1]} caption={data[2]}/>
-            <Vision img={data[0]} title={data[1]} caption={data[2]}/>
+                {
+                    data.map((item, key) => (
+                        <Vision
+                            img={item.img} key={key}
+                            title={item.title}
+                            caption={item.caption}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
