bb7d14ce4b34cdee75cdd58046dac466bcfaa295
Manan Doshi <manandoshi1301@gmail.com>
2021-11-19 16:36:51 +0530

    Added Footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../CommonComp/Footer/Footer';
 import Nav from '../CommonComp/Nav/Nav'
 import Banner from './Banner/Banner';
 import styles from "./Home.module.scss"
@@ -13,6 +14,7 @@ const Home = () => {
         <Nav />
       </div>
       <Banner />
+      <Footer />
     </div>
   )
 }
