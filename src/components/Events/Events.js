29c2d3c89bcee1b54c1738e3d36380c751db419e
Manan Doshi <manandoshi1301@gmail.com>
2021-11-24 01:09:35 +0530

    Added prev session recording

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,13 +2,15 @@ import React from 'react'
 import cx from "classnames";
 import Nav from '../CommonComp/Nav/Nav'
 import styles from './Events.module.scss'
+import PrevSessionRec from './PrevSessionRec/PrevSessionRec';
 
 const Events = () => {
   return (
-    <div>
+    <div className={styles.events}>
       <Nav />
-      <div className={cx(styles.Events)}>
-
+      <div className={cx(styles.eventsBody)}>
+        <div className={styles.eventsTitle}>Events</div>
+        <PrevSessionRec />
       </div>
 
     </div>
