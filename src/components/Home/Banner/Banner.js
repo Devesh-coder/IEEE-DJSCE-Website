ddcc498d37f33292889070d7e9b3927c4d18a72b
Manan Doshi <manandoshi1301@gmail.com>
2021-11-23 12:27:04 +0530

    Added typewriter effect

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./Banner.module.scss";
 import cx from "classnames";
 import img from "../../../images/Banner_withoutBg_cropped.png";
+import ReactTypingEffect from 'react-typing-effect';
 
 const Banner = () => {
 
@@ -17,9 +18,24 @@ const Banner = () => {
       <div className={styles.bannerText}>
 
         <div className={styles.statement}>
-          <h1 className={cx(styles.statElement, styles.envision)}>Envision</h1>
+          <ReactTypingEffect
+            text={["Envision Execute"]}
+            cursorRenderer={cursor => (<h1>{cursor}</h1>)}
+            className={cx(styles.statElement, styles.envision)}
+          />
+          {/* <ReactTypingEffect
+            text={["Execute"]}
+            cursorRenderer={cursor => (<h1>{cursor}</h1>)}
+            className={cx(styles.statElement, styles.execute)}
+          /> */}
+          <ReactTypingEffect
+            text={["Excel "]}
+            cursorRenderer={cursor => (<h1>{cursor}</h1>)}
+            className={cx(styles.statElement, styles.excel)}
+          />
+          {/* <h1 className={cx(styles.statElement, styles.envision)}>Envision</h1>
           <h1 className={cx(styles.statElement, styles.execute)}>Execute</h1>
-          <h1 className={cx(styles.statElement, styles.excel)}>Excel</h1>
+          <h1 className={cx(styles.statElement, styles.excel)}>Excel</h1> */}
         </div>
         <h3 className={styles.description}>
           IEEE Brainwaves is the official IEEE Student branch of Dwarkadas J. Sanghvi College of Engineering. <br /> We believe in leveraging technology for better tomorrow.
