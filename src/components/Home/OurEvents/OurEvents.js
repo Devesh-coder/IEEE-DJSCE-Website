c214875232efeb8c5e348d88ced75b244df87586
PrachetShah <prachetshah25@gmail.com>
2021-11-26 08:46:45 +0530

    added dumy carousel

--- src/components/Home/OurEvents/OurEvents.js
+++ src/components/Home/OurEvents/OurEvents.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from "./OurEvents.module.scss";
+import CarouselComp from "./Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
 
 const OurEvents = () => {
+
+  
   return (
     <div className={styles.ourEvents}>
       <h1 className={styles.eventHeading}>Events</h1>
       <div>
-        <button>2017</button>
+        <button onClick={() => {}}>2017</button>
         <button>2018</button>
         <button>2019</button>
         <button>2020</button>
         <button>2021</button>
+      </div>
+      <div className={styles.Carousel}>
+        <CarouselComp />
       </div>
     </div>
   )
