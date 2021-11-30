01cb42076a0cb8c4ab4ce6c2d61f0b4606123b4e
Manan Doshi <manandoshi1301@gmail.com>
2021-11-30 12:10:53 +0530

    Formatted some code

--- src/components/Team/Profile/Profile.js
+++ src/components/Team/Profile/Profile.js
@@ -2,23 +2,29 @@ import React from 'react'
 import styles from "./Profile.module.scss";
 
 function Profile({ name, title, img, jrs }) {
+
+    const ToolTipComp = () => {
+
+        const comp = (
+            jrs.length > 0 ? (
+                <div className={styles.profile__tooltip}>
+                    {
+                        jrs.map((jr) => {
+                            return <div>{jr}</div>
+                        })
+                    }
+                </div>
+            ) : null)
+
+        return comp;
+    }
+
     return (
         <div className={styles.profile}>
             <div className={styles.profile__background}>
                 <img src={img} alt=""></img>
             </div>
-            { 
-            jrs.length > 0 ? (
-            <div className={styles.profile__tooltip}>
-                {
-                    jrs.map((jr)=>{
-                        return <div>{jr}</div>
-                    })
-                }
-            </div>)
-            :
-            (<span></span>)
-            }
+            <ToolTipComp />
             <h3 className={styles.profile__name}>{name}</h3>
             <h5 className={styles.profile__title}>{title}</h5>
         </div>
