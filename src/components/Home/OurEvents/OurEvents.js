9a480b70607897643a6472669095217f92147e9d
Manan Doshi <manandoshi1301@gmail.com>
2021-12-07 14:02:02 +0530

    Updated import

--- src/components/Home/OurEvents/OurEvents.js
+++ src/components/Home/OurEvents/OurEvents.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from "./OurEvents.module.scss";
-import CarouselComp from "./Carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import CarouselComp from "./Carousel/Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const OurEvents = () => {
 
-  
+
   return (
     <div className={styles.ourEvents}>
       <h1 className={styles.eventHeading}>Events</h1>
       <div>
-        <button onClick={() => {}}>2017</button>
+        <button onClick={() => { }}>2017</button>
         <button>2018</button>
         <button>2019</button>
         <button>2020</button>
