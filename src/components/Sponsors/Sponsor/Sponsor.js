4b5885a7e976fe43cb1e558111e2870aa97b5444
Manan Doshi <manandoshi1301@gmail.com>
2021-12-13 00:49:01 +0530

    Added target in link

--- src/components/Sponsors/Sponsor/Sponsor.js
+++ src/components/Sponsors/Sponsor/Sponsor.js
@@ -2,12 +2,12 @@ import React from "react";
 import styles from './Sponsor.module.scss';
 
 
-export default function Sponsor({img, website, name}) {
+export default function Sponsor({ img, website, name }) {
     return (
         <div className={styles.sponsorBase}>
             <img className={styles.sponsorImg} src={img} alt="" />
             <h2 className={styles.sponsorName}>
-                <a href={website} className={styles.sponsorLink}>{name}</a>
+                <a href={website} className={styles.sponsorLink} target={"_blank"} rel={"noopener noreferrer"}>{name}</a>
             </h2>
         </div>
     )
