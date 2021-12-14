654a6413d502e11ecc1f57a3f5f3bb138cc279f4
Manan Doshi <manandoshi1301@gmail.com>
2021-12-14 16:04:11 +0530

    Updated sponsors page

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -3,18 +3,36 @@ import Nav from '../CommonComp/Nav/Nav'
 import Footer from '../CommonComp/Footer/Footer'
 import Sponsor from './Sponsor/Sponsor'
 import styles from "./Sponsors.module.scss"
+import cob_img from "../../images/Sponsors/codingBlock_logo.png"
+import con_img from "../../images/Sponsors/codingNinja_logo.svg"
+import inb_img from "../../images/Sponsors/InterviewBuddy_logo.png"
+import gfg_img from "../../images/Sponsors/gfg_.png"
 import afs_img from "../../images/Sponsors/a-fragrance-story-logo.png"
 import bal_img from "../../images/Sponsors/Bade Log.png"
-import cob_img from "../../images/Sponsors/Coding Blocks.png"
-import con_img from "../../images/Sponsors/Coding Ninjas.png"
 import ddm_img from "../../images/Sponsors/Dudeme.png"
-import gfg_img from "../../images/Sponsors/gfg_.png"
-import inb_img from "../../images/Sponsors/InterviewBuddy_Logo_White (1).png"
 import prb_img from "../../images/Sponsors/Prep Bytes.png"
-import spb_img from "../../images/Sponsors/Spin Bot.png"
+import spb_img from "../../images/Sponsors/spinbot_logo.jpg"
 import syb_img from "../../images/Sponsors/SYBGEN-1.png"
 
 const data = [
+  {
+    img: con_img,
+    website: "https://www.codingninjas.com/",
+    email: "",
+    name: "Coding Ninjas"
+  },
+  {
+    img: cob_img,
+    website: "https://codingblocks.com/",
+    email: "",
+    name: "Coding Blocks"
+  },
+  {
+    img: gfg_img,
+    website: "https://www.geeksforgeeks.org/",
+    email: "campus@geeksforgeeks.org",
+    name: "GeeksforGeeks"
+  },
   {
     img: afs_img,
     website: "https://afragrancestory.in/",
@@ -28,28 +46,10 @@ const data = [
     name: "Bade Log"
   },
   {
-    img: cob_img,
-    website: "https://codingblocks.com/",
-    email: "",
-    name: "Coding Blocks"
-  },
-  {
-    img: con_img,
-    website: "https://www.codingninjas.com/",
-    email: "",
-    name: "Coding Ninjas"
-  },
-  {
     img: ddm_img,
     website: "https://www.dudeme.in/",
     email: "",
     name: "Dude Me"
-  },
-  {
-    img: gfg_img,
-    website: "https://www.geeksforgeeks.org/",
-    email: "campus@geeksforgeeks.org",
-    name: "GeeksforGeeks"
   },
   {
     img: inb_img,
@@ -83,11 +83,11 @@ const Sponsors = () => {
       <Nav />
       <h1 className={styles.ourSponsors}>Our Sponsors</h1>
       <div className={styles.sponsorContainer}>
-      {
-        data.map((item,key) => (
-          <Sponsor img={item.img} key={key} website={item.website} name={item.name} />
-        ))
-      }
+        {
+          data.map((item, key) => (
+            <Sponsor img={item.img} key={key} website={item.website} name={item.name} />
+          ))
+        }
       </div>
       <Footer />
     </div>
